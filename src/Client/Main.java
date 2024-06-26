f2fadbc95101de38abcbf96348916b4bd56f3517
Jihye Lee <zerom980706@gmail.com>
2024-06-26 20:57:38 +0900

    클라이언트 5명 동시 실행

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -5,7 +5,11 @@ import java.io.PrintWriter;
 public class Main {
     private static PrintWriter writer;
     public static void main(String[] args) {
-        //EnterGui 실행
+        //EnterGui 실행 - 5명 동시 실행 가능
+        new EnterGui();
+        new EnterGui();
+        new EnterGui();
+        new EnterGui();
         new EnterGui();
     }
 }
